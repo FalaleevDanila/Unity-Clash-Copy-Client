9a9ccf8cf53b9a58a7d181be4af44c0a292b721e
FalaleevDanila <falaleevdanila@gmail.com>
2026-02-01 00:36:40 +0300

    add archer animation

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,14 +1,14 @@
 using System;
 using UnityEngine;
 
-[RequireComponent(typeof(UnitParameters), typeof(Health))]
+[RequireComponent(typeof(UnitParameters), typeof(Health), typeof(UnitAnimation))]
 public class Unit : MonoBehaviour, IHealth, IDestroyed
 {
     public event Action Destroyed;
     [field: SerializeField] public Health health { get; private set; }
     [field: SerializeField] public bool isEnemy { get; private set; } = false;
     [field: SerializeField] public UnitParameters parameters;
-    //[SerializeField] private UnitAnimation _animation;
+    [SerializeField] private UnitAnimation _animation;
     [SerializeField] private UnitState _defaultStateSO;
     [SerializeField] private UnitState _chaseStateSO;
     [SerializeField] private UnitState _attackStateSO;
@@ -19,7 +19,7 @@ public class Unit : MonoBehaviour, IHealth, IDestroyed
 
     private void Start()
     {
-        //_animation.Init(this);
+        _animation.Init(this);
 
         CreateStates();
         _currentState = _defaultState;
@@ -76,7 +76,7 @@ public class Unit : MonoBehaviour, IHealth, IDestroyed
         }
 
         _currentState.Init();
-        //_animation.SetState(type);
+        _animation.SetState(type);
     }        
 
 #if UNITY_EDITOR
