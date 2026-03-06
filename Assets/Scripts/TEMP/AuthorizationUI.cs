fb4c7496338bafd82c210808c61bac66729858a2
FalaleevDanila <falaleevdanila@gmail.com>
2026-03-07 00:54:53 +0300

    Temp

---
+++ Assets/Scripts/TEMP/AuthorizationUI.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthorizationUI : MonoBehaviour
+{
+    [SerializeField] private Authorization _authorization;
+    [SerializeField] private InputField _login;
+    [SerializeField] private InputField _password;
+    [SerializeField] private Button _signIn;
+    [SerializeField] private Button _signUp;
+
+
+    private void Awake()
+    {
+        _login.onEndEdit.AddListener(_authorization.SetLogin);
+        _password.onEndEdit.AddListener(_authorization.SetPassword);
+
+        _signIn.onClick.AddListener(SingInClick);
+
+        _authorization. Error += () =>
+        {
+            _signIn.gameObject.SetActive(true);
+            _signUp.gameObject.SetActive(true);
+        };
+    }
+
+    private void SingInClick()
+    {
+        
+        _signIn.gameObject.SetActive(false);
+        _signUp.gameObject.SetActive(false);
+        _authorization.SignIn();
+    }
+
+}
---
+++ Assets/Scripts/TEMP/URLLibrary.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class URLLibrary
+{
+    public const string MAIN = "http://localhost/Projects/";
+    public const string AUTHORIZATION = "Project/authorization.php";
+}
